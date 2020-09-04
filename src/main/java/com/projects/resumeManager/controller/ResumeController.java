4326bc3b3cf5eb02d6421510a1073883148bf874
Probsno <56311901+grove156@users.noreply.github.com>
2020-09-04 15:51:49 +0900

    google ouath instance set up

--- src/main/java/com/projects/resumeManager/controller/ResumeController.java
+++ src/main/java/com/projects/resumeManager/controller/ResumeController.java
@@ -1,7 +1,12 @@
 package com.projects.resumeManager.controller;
 
+import com.projects.resumeManager.domain.AuthUser;
 import com.projects.resumeManager.domain.entity.Resume;
+import com.projects.resumeManager.domain.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +15,13 @@ import java.util.List;
 @Controller
 public class ResumeController {
 
+    Authentication authentication = (Authentication) SecurityContextHolder.getContext().getAuthentication();
+    AuthUser user = (AuthUser) authentication.getPrincipal();
+
     @GetMapping("/dashboard")
-    public String dashboard(){
+    public String dashboard(Model model){
+
+        model.addAttribute("user",user);
         return "dashboard";
     }
 }
