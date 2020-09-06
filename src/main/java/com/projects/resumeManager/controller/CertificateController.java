b8b3906f38863313d50cee253ae73f2ab42467e2
Probsno <56311901+grove156@users.noreply.github.com>
2020-09-06 20:58:21 +0900

    structure works on crud on certificate completed

--- src/main/java/com/projects/resumeManager/controller/CertificateController.java
+++ src/main/java/com/projects/resumeManager/controller/CertificateController.java
@@ -6,10 +6,7 @@ import com.projects.resumeManager.service.CertificateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -31,7 +28,20 @@ public class CertificateController {
 
     @PostMapping("/resume/{resumeId}/certificate")
     public void createCertificate(@PathVariable(value = "resumeId") Long resumeId,
-                                  @Valid @RequestBody CertificateCreateRequest certificateCreateRequest){
-        certificateService.createCertificate(resumeId, certificateCreateRequest);
+                                  @Valid @RequestBody CertificateCreateRequest certificateCreateRequest) throws Exception {
+        CertificateDetailResponse certificateDetailResponse = certificateService.createCertificate(resumeId, certificateCreateRequest);
+    }
+
+    @PatchMapping("/resume/{resumeId}/certificate/{certificateId}")
+    public void updateCertificate(@PathVariable(value = "resumeId") Long resumeId,
+                                  @PathVariable(value = "certificateId") Long certificateId,
+                                  @RequestBody CertificateCreateRequest certificateCreateRequest) throws Exception {
+        CertificateDetailResponse certificateDetailResponse = certificateService.updateCertificate(resumeId, certificateId, certificateCreateRequest);
+    }
+
+    @DeleteMapping("/resume/{resumeId}/education/{certificateId}")
+    public void deleteCertificate(@PathVariable(value = "resumeId") Long resumeId,
+                                  @PathVariable(value = "certificateId") Long certificateId){
+        certificateService.deleteCertificate(resumeId, certificateId);
     }
 }
--- src/main/java/com/projects/resumeManager/service/CertificateService.java
+++ src/main/java/com/projects/resumeManager/service/CertificateService.java
@@ -1,6 +1,7 @@
 package com.projects.resumeManager.service;
 
 import com.projects.resumeManager.domain.entity.Certificate;
+import com.projects.resumeManager.domain.entity.Resume;
 import com.projects.resumeManager.dto.request.CertificateCreateRequest;
 import com.projects.resumeManager.dto.response.CertificateDetailResponse;
 import com.projects.resumeManager.repository.CertificateRepository;
@@ -8,6 +9,7 @@ import com.projects.resumeManager.repository.ResumeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -38,17 +40,84 @@ public class CertificateService {
                 .updatedAt(certificate.getUpdatedAt())
                 .build();
 
+        //3
         return certificateDetailResponse;
     }
 
-    public void createCertificate(Long resumeId, CertificateCreateRequest certificateCreateRequest){
-        Certificate certificate =Certificate.builder()
-                .certificateTitle()
-                .score()
-                .issuedDate()
-                .validDuration()
-                .
-                .createdAt()
+
+    //1. find resume object to chain with certificate object
+    //2. map certificate create request to certificate object
+    //3. save
+    //4. map saved certificate obejct to certificate detail response object and then return
+    public CertificateDetailResponse createCertificate(Long resumeId, CertificateCreateRequest certificateCreateRequest) throws Exception {
+        //TODO: wring resume exception create and apply instead of Exception
+
+        //1
+        Resume resume = resumeRepository.findById(resumeId).orElseThrow(()->new Exception());
+
+        //2
+        Certificate certificate = Certificate.builder()
+                .certificateTitle(certificateCreateRequest.getCertificateTitle())
+                .score(certificateCreateRequest.getScore())
+                .issuedDate(certificateCreateRequest.getIssuedDate())
+                .validDuration(certificateCreateRequest.getValidDuration())
+                .createdAt(LocalDateTime.now())
+                .resume(resume)
                 .build();
+
+        //3
+        Certificate newCertificate = certificateRepository.save(certificate);
+
+        //4
+        CertificateDetailResponse certificateDetailResponse = CertificateDetailResponse.builder()
+                .id(newCertificate.getId())
+                .certificateTitle(newCertificate.getCertificateTitle())
+                .score(newCertificate.getScore())
+                .issueDate(newCertificate.getIssuedDate())
+                .validDuration(newCertificate.getValidDuration())
+                .createdAt(newCertificate.getCreatedAt())
+                .build();
+
+        return certificateDetailResponse;
+    }
+
+    //1. get certificate with certificate id
+    //2. set found instance of certificate with instance of certificate update request
+    //3. save updated certificate
+    //4. map updated certificate with certificate detail response object  return certificate detail response object
+    public CertificateDetailResponse updateCertificate(Long resumeId,
+                                                       Long certificateId,
+                                                       CertificateCreateRequest certificateCreateRequest) throws Exception {
+        //TODO: writing and apply certificate not found exception
+
+        //1
+        Certificate certificate = certificateRepository.findById(certificateId).orElseThrow(()->new Exception());
+
+        //2
+        certificate.setCertificateTitle(certificateCreateRequest.getCertificateTitle());
+        certificate.setScore(certificateCreateRequest.getScore());
+        certificate.setIssuedDate(certificateCreateRequest.getIssuedDate());
+        certificate.setValidDuration(certificateCreateRequest.getValidDuration());
+        certificate.setUpdatedAt(LocalDateTime.now());
+
+        //3
+        Certificate updatedCertificate = certificateRepository.save(certificate);
+
+        //4
+        CertificateDetailResponse certificateDetailResponse = CertificateDetailResponse.builder()
+                .id(updatedCertificate.getId())
+                .certificateTitle(updatedCertificate.getCertificateTitle())
+                .score(updatedCertificate.getScore())
+                .issueDate(updatedCertificate.getIssuedDate())
+                .validDuration(updatedCertificate.getValidDuration())
+                .createdAt(updatedCertificate.getCreatedAt())
+                .updatedAt(updatedCertificate.getUpdatedAt())
+                .build();
+
+        return certificateDetailResponse;
+    }
+
+    public void deleteCertificate(Long resumeId, Long certificateId) {
+        certificateRepository.deleteById(certificateId);
     }
 }
